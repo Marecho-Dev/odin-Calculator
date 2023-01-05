cb9721cc5edf1c7cdf677810e16e6f813902663e
Mareks1993 <marekstaszkiewicz@live.com>
2023-01-04 22:01:48 -0500

    updated the class names of my button divs to make it easier to differentiate between operators and numbers

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let operator = "";
-let currentValue = 0;
-let display;
+let currentValue = [];
+let display = "";
 function operate(operator,x,y){
 
 }
 function changeDisplay(x){
+    if(display != ""){
+        console.log(display);
+        display = display+x;
+    }
+    else{
+        console.log("'" + display + "'");
     display = x;
+    }
+    const output = document.querySelector('.Output');
+    output.textContent = display;
 }
-const buttons = document.querySelectorAll('.left');
+const buttons = document.querySelectorAll('.number');
 buttons.forEach((button)=>{
     button.addEventListener('click', ()=> {
         changeDisplay(button.textContent);
+        console.log(display);
+        
     });
 });
