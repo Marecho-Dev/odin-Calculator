df5a0166c9d7061c7568367552a3aaaf7980427e
Mareks1993 <marekstaszkiewicz@live.com>
2023-01-04 21:26:29 -0500

    added querySelector for buttons which will pass the text content over to changeDisplay function.

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ let display;
 function operate(operator,x,y){
 
 }
-
+function changeDisplay(x){
+    display = x;
+}
+const buttons = document.querySelectorAll('.left');
+buttons.forEach((button)=>{
+    button.addEventListener('click', ()=> {
+        changeDisplay(button.textContent);
+    });
+});
