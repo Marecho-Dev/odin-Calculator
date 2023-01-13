6f7d24d0ac99bbf4e396ec7f4a315e1a59eacc78
Mareks1993 <marekstaszkiewicz@live.com>
2023-01-13 00:02:01 -0500

    expanded stringBuilder function to output answer and have functionality for repeated oeprator inputs.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let operator = "";
 let display = "";
 let previousValue = "";
 let currentValue = "";
+let answer = "";
 let decimalCounter = 0;
 let operatorCounter = 0;
 
@@ -10,31 +11,58 @@ function operate(operator,x,y){
 }
 
 function stringBuilder(x,buttonType){
+    console.log("----------------");
     if (x=="."){
+        console.log("calling part 1");
         if (decimalCounter == 0){
             currentValue = currentValue + x;
             decimalCounter+=1;
         }
     }
-    else if (buttonType=="number") currentValue = currentValue + x;
+    else if (buttonType=="number") {
+        console.log("calling part 2");
+        currentValue = currentValue + x;
+    }
     else if (buttonType=="operator"){
+        console.log("calling part 3");
         if (operatorCounter == 0){
+            console.log(operatorCounter);
             operator = x;
             previousValue = currentValue;
             currentValue = "";
             operatorCounter+=1;
             decimalCounter=0;
         }
+        else{
+            console.log(operatorCounter);
+            console.log("the operator is " + operator);
+            previousValue = calculate(operator,previousValue,currentValue);
+            answer = "";
+            currentValue = "";
+            operator = x;
+            decimalCounter = 0;
+
+        }
     }
     else if (buttonType=="equal"){
-        console.log(calculate(operator,previousValue,currentValue));
+        console.log("calling part 4");
+        console.log(operator);
+        console.log(previousValue);
+        console.log(currentValue);
+        answer = "="+calculate(operator,previousValue,currentValue);
+
     }
+    
+    console.log(previousValue + " is the previous Value");
+    console.log(currentValue + " is the current value");
+    console.log("the operator is " + operator);
+    console.log("---------------");
     changeDisplay();
 }
 
 function changeDisplay(){
     const output = document.querySelector('.Output');
-    output.textContent = previousValue+operator+currentValue;
+    output.textContent = previousValue+operator+currentValue+answer;
 }
 
 function parser(x){
